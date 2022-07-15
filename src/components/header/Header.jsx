9bdbfd8e55e2913413add9cb98322fd86088c511
Ислам Мадаминов <isxmad@gmail.com>
2022-07-15 17:47:31 +0300

    gamerules

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -2,8 +2,8 @@ import './Header.scss';
 import Button from '../button/Button';
 import { useState } from 'react';
 import {Modal} from 'antd';
-import img from '../../images/game-rules.png';
 import '../gameRules/GameRules.scss';
+import road from '../../images/road.svg';
 
 const Header = () => {
     const [connected, setConnected] = useState(true);
@@ -27,7 +27,70 @@ const Header = () => {
                         </div>
                     </div>
                     <div className="gameRules__body">
-                        <img src={img} alt="" />
+                        <div className="gameRules__body_road">
+                            <img src={road} alt={road} />
+                        </div>
+                        <div className="gameRules__body_item gameRules__body_item-1">
+                            <div className="gameRules__body_item_num">1</div>
+                            <div className="gameRules__body_item_text">
+                                When the game is launched from 1 to 21 balls, 
+                                the activation of the balls occurs according 
+                                to the next. 
+                            </div>
+                        </div>
+                        <div className="gameRules__body_item gameRules__body_item-2">
+                            <div className="gameRules__body_item_num">2</div>
+                            <div className="gameRules__body_item_text">
+                                Every time the ball makes 
+                                a circle, you get a reward 
+                                of 70% of the cost of the ball, but no more than 
+                                4 times, then the ball must 
+                                be activated again. If you 
+                                do not activate the bursting ball, the rest of the balls freeze
+                            </div>
+                        </div>
+                        <div className="gameRules__body_item gameRules__body_item-3">
+                            <div className="gameRules__body_item_num">3</div>
+                            <div className="gameRules__body_item_text">
+                                If at the first passage from 
+                                1 to 21, your next ball is not activated, then the last ball will be able to make only 2 circles, then all balls will be frozen. 
+                                To defrost them, you need 
+                                to activate the next
+                                ball in turn 
+                            </div>
+                        </div>
+                        <div className="gameRules__body_item gameRules__body_item-4">
+                            <div className="gameRules__body_item_num">4</div>
+                            <div className="gameRules__body_item_text">
+                            After activating all balls, you need to maintain their total amount on the monitor, each time activating the bursting balls again
+                            </div>
+                        </div>
+                        <div className="gameRules__body_item gameRules__body_item-5">
+                            <div className="gameRules__body_item_num">5</div>
+                            <div className="gameRules__body_item_text">
+                                All funds are instantly distributed 
+                                to players' wallets. When your balls are frozen, the remuneration remains on the smart contract until you activate the ball you need 
+                                for defrosting. For activation after freezing, you are given 72 hours, after which your reward will 
+                                be distributed by other player
+                            </div>
+                        </div>
+                        <div className="gameRules__body_item gameRules__body_item-6">
+                            <div className="gameRules__body_item_num">6</div>
+                            <div className="gameRules__body_item_text">
+                                Inviting new players at your referral link, you will get 
+                                an additional bonus of 15% 
+                                of the cost of each ball that you will ever acquire 
+                                the player you invited, as well as 10% of the second line 
+                                and 5% of the third players' line invited by your partners
+                            </div>
+                        </div>
+                        <div className="gameRules__body_item gameRules__body_item-7">
+                            <div className="gameRules__body_item_num">7</div>
+                            <div className="gameRules__body_item_text">
+                                For the start, you need 
+                                to buy the first ball worth 0.05 BNB.
+                            </div>
+                        </div>
                     </div>
                     <div className="gameRules__foot">
                         <div className="gameRules__foot_btn">
